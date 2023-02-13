fc98fd17e63a64fa6008742cb55ab20f0cbb9a1e
Brandon Woods <woods.brandon.m@gmail.com>
2023-02-13 10:49:23 -0800

    Pass all tests for Grub class pseudoclassical

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,13 @@
 var Grub = function() {
+  // age
+  this.age = 0;
+  // color
+  this.color = 'pink';
+  // food
+  this.food = 'jelly';
 };
 
+// eat method
+Grub.prototype.eat = function () {
+
+};
