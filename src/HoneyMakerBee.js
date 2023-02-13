642d415f67bbf7e9bdb5f5382441d0f095deef52
Brandon Woods <woods.brandon.m@gmail.com>
2023-02-13 11:08:17 -0800

    Pass all tests for HoneyMakerBee pseudoclassical

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  // call Bee class
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  return this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  return this.honeyPot -= 1;
+};
